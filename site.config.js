f60fdf4cd1c9d767958be3f07027f0aabb61cf19
2now <kj109888@gmail.com>
2022-01-27 23:23:36 +0900

    chore: site.config.js edit

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '762a0bbfb4684cccacf6b17a243031de',
+  // 762a0bbfb4684cccacf6b17a243031de
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: '지금',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: '2-NOW',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: '2-NOW',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: '2-NOW',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
